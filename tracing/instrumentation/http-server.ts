b650a63ec4f5c006c6f5c56419d06946af74f524
Daniel Lamando <dan@danopia.net>
2023-03-13 18:20:11 +0100

    Add two metrics to instrumentation/http-server.ts

--- tracing/instrumentation/http-server.ts
+++ tracing/instrumentation/http-server.ts
@@ -1,25 +1,43 @@
 import { NetTransportValues, SemanticAttributes } from "https://esm.sh/@opentelemetry/semantic-conventions@1.9.1";
 import {
+  metrics,
   propagation,
   ROOT_CONTEXT,
   SpanKind,
+  trace,
+  ValueType,
   type TextMapGetter,
-  type TracerProvider,
 } from "../api.ts";
 
-export function httpTracer(provider: TracerProvider, inner: Handler): Handler {
-  const tracer = provider.getTracer('http');
-  return async (req: Request, connInfo: ConnInfo) => {
+export function httpTracer(inner: Handler): Handler {
 
+  // https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/http.md
+  const tracer = trace.getTracer('http');
+
+  // https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/semantic_conventions/http-metrics.md
+  const myMeter = metrics.getMeter('http');
+  const durationMetric = myMeter.createHistogram('http.server.duration', {valueType: ValueType.DOUBLE});
+  const inflightMetric = myMeter.createCounter('http.server.active_requests', {valueType: ValueType.INT});
+
+  return async (req: Request, connInfo: ConnInfo) => {
+    const d0 = performance.now();
     const url = new URL(req.url);
+
+    const reqMetricAttrs = {
+      [SemanticAttributes.HTTP_SCHEME]: url.protocol.split(':')[0],
+      [SemanticAttributes.HTTP_METHOD]: req.method,
+      [SemanticAttributes.NET_HOST_NAME]: url.host, // not sure why metrics spec wants it this way
+    };
+    inflightMetric.add(1, reqMetricAttrs);
+
     const ctx = propagation.extract(ROOT_CONTEXT, req.headers, HeadersGetter);
     return tracer.startActiveSpan(`${req.method} ${url.pathname}`, {
       kind: SpanKind.SERVER,
       attributes: {
+        [SemanticAttributes.HTTP_SCHEME]: url.protocol.split(':')[0],
         [SemanticAttributes.HTTP_METHOD]: req.method,
         [SemanticAttributes.HTTP_URL]: req.url,
         [SemanticAttributes.HTTP_HOST]: url.host,
-        [SemanticAttributes.HTTP_SCHEME]: url.protocol.split(':')[0],
         [SemanticAttributes.HTTP_USER_AGENT]: req.headers.get('user-agent') ?? undefined,
         [SemanticAttributes.HTTP_ROUTE]: url.pathname, // for datadog
         // 'http.request_content_length': '/http/request/size',
@@ -34,10 +52,12 @@ export function httpTracer(provider: TracerProvider, inner: Handler): Handler {
             [SemanticAttributes.NET_HOST_PORT]: connInfo.localAddr.port,
             [SemanticAttributes.NET_PEER_IP]: connInfo.remoteAddr.hostname,
             [SemanticAttributes.NET_PEER_PORT]: connInfo.remoteAddr.port,
+            ['net.sock.family']: connInfo.remoteAddr.hostname.includes(':') ? 'inet6' : 'inet',
           })
         } else if (connInfo.localAddr.transport == 'unix' || connInfo.localAddr.transport == 'unixpacket') {
           serverSpan.setAttributes({
             [SemanticAttributes.NET_TRANSPORT]: NetTransportValues.UNIX,
+            ['net.sock.family']: 'unix',
           })
         }
 
@@ -53,14 +73,20 @@ export function httpTracer(provider: TracerProvider, inner: Handler): Handler {
         const respSnoop = snoopStream(resp.body);
         respSnoop.finalSize.then(size => {
           serverSpan.setAttribute(SemanticAttributes.HTTP_RESPONSE_CONTENT_LENGTH_UNCOMPRESSED, size);
-        }).finally(() => serverSpan.end());
+        }).finally(() => {
+          inflightMetric.add(-1, reqMetricAttrs);
+          serverSpan.end();
+        });
 
         return new Response(respSnoop.newBody, resp)
 
       } catch (err) {
         serverSpan.recordException(err);
         serverSpan.end();
+        inflightMetric.add(-1, reqMetricAttrs);
         throw err;
+      } finally {
+        durationMetric.record(performance.now() - d0, reqMetricAttrs, ctx);
       }
     });
   };
