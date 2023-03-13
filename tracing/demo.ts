2171381b8cdf0f30e6c922f527f83ce374c9a197
Daniel Lamando <dan@danopia.net>
2023-03-13 17:27:09 +0100

    Update demo with more examples

--- tracing/demo.ts
+++ tracing/demo.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run --watch --allow-read --allow-sys=hostname --allow-env --allow-net --allow-run=uptime
+#!/usr/bin/env -S deno run --watch --allow-read --allow-sys=hostname --allow-env --allow-net --allow-run=uptime,sleep,ping
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
 import { DenoFetchInstrumentation } from './instrumentation/fetch.ts';
@@ -30,9 +30,6 @@ async function handler(req: Request): Promise<Response> {
   const url = new URL(req.url);
   console.log(req.method, url.pathname);
 
-  if (url.pathname == '/favicon.ico') {
-    return new Response('404', { status: 404 });
-  }
   if (url.pathname == '/inner') {
     await getData();
     return new Response(JSON.stringify({
@@ -42,23 +39,62 @@ async function handler(req: Request): Promise<Response> {
   }
 
   if (url.pathname == '/uptime') {
+    // const text = await new Promise<string>(ok => setTimeout(async () => {
+      const proc = Deno.run({
+        cmd: ['uptime'],
+        stdin: 'null',
+        stdout: 'piped',
+        stderr: 'inherit',
+      });
+      const text = await new Response(proc.stdout.readable).text();
+      await proc.status();
+      // ok(text);
+    // }, 50));
+    return new Response(text);
+  }
+
+  if (url.pathname == '/sleep') {
     const proc = Deno.run({
-      cmd: ['uptime'],
+      cmd: ['sleep', '1'],
+      stdin: 'null',
+    });
+    await proc.status();
+    await Promise.all([1,2,3,4,5].map(x => {
+      const proc = Deno.run({
+        cmd: ['sleep', `${x}`],
+        stdin: 'null',
+      });
+      return proc.status();
+    }));
+    const proc2 = Deno.run({
+      cmd: ['sleep', '1'],
+      stdin: 'null',
+    });
+    await proc2.status();
+    return new Response('done');
+  }
+
+  if (url.pathname == '/ping') {
+    const proc = Deno.run({
+      cmd: ['ping', '-c5', 'google.com'],
       stdin: 'null',
       stdout: 'piped',
       stderr: 'inherit',
     });
-    const text = await new Response(proc.stdout.readable).text();
-    await proc.status();
-    return new Response(text);
+    proc.status();
+    return new Response(proc.stdout.readable);
   }
 
-  const body1 = await getData();
-  const body2 = await fetch('http://localhost:8000/inner').then(x => x.json());
-  return new Response(JSON.stringify({
-    headers: Object.fromEntries(req.headers),
-    body1, body2,
-  }, null, 2));
+  if (url.pathname == '/') {
+    const body1 = await getData();
+    const body2 = await fetch('http://localhost:8000/inner').then(x => x.json());
+    return new Response(JSON.stringify({
+      headers: Object.fromEntries(req.headers),
+      body1, body2,
+    }, null, 2));
+  }
+
+  return new Response('404', { status: 404 });
 }
 
 async function getData() {
