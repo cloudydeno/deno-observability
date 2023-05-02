49ecea92d2106c58c7fc75ea05acc870ffbd2f38
Daniel Lamando <dan@danopia.net>
2023-05-03 00:15:47 +0200

    Clean up DIY logs otlp-transform code

--- otel-platform/otlp-exporters.ts
+++ otel-platform/otlp-exporters.ts
@@ -1,33 +1,19 @@
-import {
-  type SpanAttributes,
-  context,
-  diag,
-} from "../opentelemetry/api.js";
-import {
-  ExportResult,
-  ExportResultCode,
-  baggageUtils,
-  suppressTracing,
-} from "../opentelemetry/core.js";
-import {
-  type Resource,
-} from "../opentelemetry/resources.js";
-
+import { diag } from "../opentelemetry/api.js";
+import { baggageUtils } from "../opentelemetry/core.js";
 import {
   appendResourcePathToUrl,
-  appendRootPathToUrlIfNeeded,
   OTLPExporterBase,
   OTLPExporterError,
   type OTLPExporterConfigBase,
   parseHeaders,
 } from "../opentelemetry/otlp-exporter-base.js";
 import {
-  createExportMetricsServiceRequest,
   createExportTraceServiceRequest,
-  type IAnyValue,
-  type IExportMetricsServiceRequest,
+  createExportMetricsServiceRequest,
+  createExportLogsServiceRequest,
   type IExportTraceServiceRequest,
-  type IKeyValue,
+  type IExportMetricsServiceRequest,
+  type IExportLogsServiceRequest,
 } from "../opentelemetry/otlp-transformer.js";
 
 import type {
@@ -35,12 +21,12 @@ import type {
   SpanExporter,
 } from "../opentelemetry/sdk-trace-base.d.ts";
 import type {
+  ResourceMetrics,
+} from "../opentelemetry/sdk-metrics.d.ts";
+import type {
   ReadableLogRecord,
   LogRecordExporter,
 } from "../opentelemetry/sdk-logs.d.ts";
-import type {
-  ResourceMetrics,
-} from "../opentelemetry/sdk-metrics.d.ts";
 
 type FetchExporterOpts = OTLPExporterConfigBase & {
   defaultPath: string;
@@ -103,7 +89,7 @@ abstract class OTLPFetchExporterBase<
       return;
     }
 
-    diag.debug(`OLTP push to ${new URL(this.url).pathname} with ${items.length} spans...`);
+    diag.debug(`OLTP push to ${new URL(this.url).pathname} with ${items.length} items...`);
     fetch(this.url, {
       method: 'POST',
       body: JSON.stringify(this.convert(items)),
@@ -165,7 +151,7 @@ export class OTLPMetricsExporter extends OTLPFetchExporterBase<
 
 export class OTLPLogsExporter extends OTLPFetchExporterBase<
   ReadableLogRecord,
-  LogPayload
+  IExportLogsServiceRequest
 > implements LogRecordExporter {
   constructor(config?: OTLPExporterConfigBase) {
     super({ ...config,
@@ -174,74 +160,8 @@ export class OTLPLogsExporter extends OTLPFetchExporterBase<
     });
   }
 
-  convert(logs: ReadableLogRecord[]): LogPayload {
-    return toLogPayload(logs[0].resource, logs);
+  convert(logs: ReadableLogRecord[]) {
+    return createExportLogsServiceRequest(logs);
   }
 }
-
 // btw, events are event.name and event.domain
-
-type LogPayload = ReturnType<typeof toLogPayload>;
-function toLogPayload(resource: Resource | undefined, allLogs: ReadableLogRecord[]) {
-  return {
-    "resourceLogs": [{
-      resource: resource ? {
-        attributes: toAttributes(resource.attributes),
-        droppedAttributesCount: 0,
-      } : {},
-      scopeLogs: allLogs.map(logItem => ({
-        scope: {
-          name: logItem.instrumentationScope.name,
-          version: logItem.instrumentationScope.version,
-          // attributes: toAttributes(logItem.),
-        },
-        schemaUrl: logItem.instrumentationScope.schemaUrl,
-        logRecords: [{
-          timeUnixNano: `${logItem.hrTime[0]}${logItem.hrTime[1].toString().padStart(9, '0')}`,
-          severityNumber: logItem.severityNumber,
-          severityText: logItem.severityText,
-          // name: 'TODO: logger name',
-          body: toAnyValue(logItem.body),
-          attributes: toAttributes(logItem.attributes ?? {}),
-          droppedAttributesCount: 0,
-          traceId: logItem.spanContext?.traceId,
-          spanId: logItem.spanContext?.spanId,
-        }],
-      })),
-    }],
-  };
-}
-
-
-// https://github.com/open-telemetry/opentelemetry-js/blob/ecb5ebe86eebc5a1880041b2523adf5f6d022282/experimental/packages/otlp-transformer/src/common/internal.ts#L19
-function toAttributes(attributes: SpanAttributes): IKeyValue[] {
-  return Object.keys(attributes).map(key => toKeyValue(key, attributes[key]));
-}
-function toKeyValue(key: string, value: unknown): IKeyValue {
-  return {
-    key: key,
-    value: toAnyValue(value),
-  };
-}
-function toAnyValue(value: unknown): IAnyValue {
-  const t = typeof value;
-  if (t === 'string') return { stringValue: value as string };
-  if (t === 'number') {
-    if (!Number.isInteger(value)) return { doubleValue: value as number };
-    return { intValue: value as number };
-  }
-  if (t === 'boolean') return { boolValue: value as boolean };
-  if (value instanceof Uint8Array) return { bytesValue: value };
-  if (Array.isArray(value))
-    return { arrayValue: { values: value.map(toAnyValue) } };
-  if (t === 'object' && value != null)
-    return {
-      kvlistValue: {
-        values: Object.entries(value as object).map(([k, v]) =>
-          toKeyValue(k, v)
-        ),
-      },
-    };
-
-  return {};
-}
