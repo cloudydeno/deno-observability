77eaa1cbd9b2e77b28499366f13139ad9c748203
Daniel Lamando <dan@danopia.net>
2024-05-11 17:38:09 +0200

    Revert "Log another potential error site"
    This reverts commit 2d352f54dd60c6b9ba734693ce2abce072d6be22.

--- instrumentation/http-server.ts
+++ instrumentation/http-server.ts
@@ -102,7 +102,6 @@ export function httpTracer(inner: Deno.ServeHandler, opts?: {
         serverSpan.recordException(err);
         serverSpan.end();
         inflightMetric.add(-1, reqMetricAttrs);
-        console.error('httpTracer Error:', err);
         throw err;
       } finally {
         durationMetric.record(performance.now() - d0, reqMetricAttrs, ctx);
